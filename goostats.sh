81f70194a3a0185860b2eb6a0f8d7a0d0f4d7662
Asekhaen <asekhaenj@gmail.com>
2024-09-06 11:56:41 +0800

    used supplied filename

--- goostats.sh
+++ goostats.sh
@@ -1,6 +1,6 @@
 #Load a given file
 #Computer the min/max/range of value in a file
 fname=$1
-echo "working with file"
+echo "working with file : ${fname}"
 echo "working to my scripts"
 # Compute the min/max/range of value in a file
